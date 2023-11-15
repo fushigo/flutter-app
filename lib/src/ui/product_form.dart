e65747a69e7dc7fedb745fc7299a856e6d66fbeb
Fushigo <rizky23ska@gmail.com>
2023-11-16 06:00:04 +0700

    update product

--- lib/src/ui/product_form.dart
+++ lib/src/ui/product_form.dart
@@ -29,42 +29,58 @@ class InputForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        TextField(
-          decoration: const InputDecoration(
-              labelText: "Kode Produk",
-              enabledBorder:
-                  UnderlineInputBorder(borderSide: BorderSide(width: 1))),
-          controller: _kodeProdukTextController,
-        ),
-        TextField(
-          decoration: const InputDecoration(
-              labelText: "Nama Produk",
-              enabledBorder:
-                  UnderlineInputBorder(borderSide: BorderSide(width: 1))),
-          controller: _namaProdukTextController,
-        ),
-        TextField(
-          decoration: const InputDecoration(
-              labelText: "Harga Produk",
-              enabledBorder:
-                  UnderlineInputBorder(borderSide: BorderSide(width: 1))),
-          controller: _hargaProdukTextController,
-        ),
-        ElevatedButton(
-            onPressed: () {
-              String kodeProduk = _kodeProdukTextController.text;
-              String namaProduk = _namaProdukTextController.text;
-              int hargaProduk = int.parse(_hargaProdukTextController.text);
-
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => ProductDetails(
-                        kodeProduk: kodeProduk,
-                        namaProduk: namaProduk,
-                        hargaProduk: hargaProduk,
-                      )));
-            },
-            child: const Text("Simpan"))
+        _kodeProduk(),
+        _namaProduk(),
+        _hargaProduk(),
+        _buttonSimpan(context)
       ],
+    );
+  }
+
+  _buttonSimpan(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          String kodeProduk = _kodeProdukTextController.text;
+          String namaProduk = _namaProdukTextController.text;
+          int hargaProduk = int.parse(_hargaProdukTextController.text);
+
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => ProductDetails(
+                    kodeProduk: kodeProduk,
+                    namaProduk: namaProduk,
+                    hargaProduk: hargaProduk,
+                  )));
+        },
+        child: const Text("Simpan"));
+  }
+
+  _hargaProduk() {
+    return TextField(
+      decoration: const InputDecoration(
+          labelText: "Harga Produk",
+          enabledBorder:
+              UnderlineInputBorder(borderSide: BorderSide(width: 1))),
+      controller: _hargaProdukTextController,
+    );
+  }
+
+  _namaProduk() {
+    return TextField(
+      decoration: const InputDecoration(
+          labelText: "Nama Produk",
+          enabledBorder:
+              UnderlineInputBorder(borderSide: BorderSide(width: 1))),
+      controller: _namaProdukTextController,
+    );
+  }
+
+  _kodeProduk() {
+    return TextField(
+      decoration: const InputDecoration(
+          labelText: "Kode Produk",
+          enabledBorder:
+              UnderlineInputBorder(borderSide: BorderSide(width: 1))),
+      controller: _kodeProdukTextController,
     );
   }
 }
